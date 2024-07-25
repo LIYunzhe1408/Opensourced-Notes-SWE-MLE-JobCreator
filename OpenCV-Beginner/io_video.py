7030e4a22d26dcf9e3fa8ae48185af2b8e2d9342
YunzheLi <liyunzhe.jonas@berkeley.edu>
2024-07-25 14:53:06 +0800

    Create python scripts for resizing and crop.
    To test crop and resizing, upload a new rune png.To align with the
    new data folder structure, the path in io_video.py has been changed.

--- OpenCV-Beginner/io_video.py
+++ OpenCV-Beginner/io_video.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 
-video_path = os.path.join('.', 'rune', '大能量机关8m正在激活状态灯效.mp4')
+video_path = os.path.join('.', 'data', 'video-rune', '大能量机关8m正在激活状态灯效.mp4')
 video = cv2.VideoCapture(video_path)
 
 ret = True
@@ -9,7 +9,8 @@ while ret:
     ret, frame = video.read()
     if ret:
         cv2.imshow('Rune', frame)
-        cv2.waitKey(40)
+        if cv2.waitKey(40) & 0xFF == ord('q'):
+            break
 
 video.release()
 cv2.destroyAllWindows()
