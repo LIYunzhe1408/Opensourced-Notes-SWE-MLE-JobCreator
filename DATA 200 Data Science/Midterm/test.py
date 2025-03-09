b1be955aec7ad266d81276833093e8a0f05da856
YunzheLi <liyunzhe.jonas@berkeley.edu>
2025-03-08 18:24:46 -0800

    [DATA200] Complete su23 midterm.

--- DATA 200 Data Science/Midterm/test.py
+++ DATA 200 Data Science/Midterm/test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 foods_df = pd.DataFrame({'Name': ["Vasanth", "Shiangyi", "Rohan", "hi"], "Age": [10, 9, 5, 4], "Height":[3.2, 2.6, 2.1, 400], "Birth Month": ["Nov", "Oct", "Dec", "Dec"]})
-
-print(foods_df.groupby("Birth Month").filter(lambda sf: sf["Height"].mean() < 5))
+print(foods_df)
+print(foods_df.groupby("Birth Month")["Age"].agg('count'))
+# print(foods_df.groupby("Birth Month").filter(lambda sf: sf["Height"].mean() < 5))
